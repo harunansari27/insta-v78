5f8b91f141323a66ab624556d235f6f1acd45525
harun ansari <harunansaru051@gmail.com>
2022-10-09 20:59:15 +0530

    add istagram logo to the left side if the header component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images:{
+    domains:["i.imgur.com","upload.wikimedia.org"]
+  },
+ 
+};
 
 module.exports = nextConfig
