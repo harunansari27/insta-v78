6c09c7b30f105a7922beaa646a7f1f3da43a2d50
harun ansari <harunansaru051@gmail.com>
2022-10-11 21:48:14 +0530

    popular to take fake data usiing minifaker and map create feed & story component

---
+++ Components/Feed.js
@@ -0,0 +1,18 @@
+import Stories from "./Stories";
+
+export default function Feed() {
+  return ( <main>
+    <section>
+        {/* Stories */}
+        <Stories/>
+        {/* Posts  */}
+    </section>
+
+    <section>
+        {/* Mini Profile */}
+
+        {/* Suggestion part */}
+    </section>
+  </main>
+  )
+}
